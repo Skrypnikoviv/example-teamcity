8a7f1e38ac90b37ee05ab9ac3696bf3f8d90c20c
Skrypnikoviv <162264420+Skrypnikoviv@users.noreply.github.com>
2025-01-16 17:03:03 +1000

    Update WelcomerTest.java

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -6,28 +6,29 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class WelcomerTest {
-	
-	private Welcomer welcomer = new Welcomer();
+   
+   private Welcomer welcomer = new Welcomer();
 
-	@Test
-	public void welcomerSaysWelcome() {
-		assertThat(welcomer.sayWelcome(), containsString("Welcome"));
-	}
-	@Test
-	public void welcomerSaysFarewell() {
-		assertThat(welcomer.sayFarewell(), containsString("Farewell"));
-	}
-	@Test
-	public void welcomerSaysHunter() {
-		assertThat(welcomer.sayWelcome(), containsString("hunter"));
-		assertThat(welcomer.sayFarewell(), containsString("hunter"));
-	}
-	@Test
-	public void welcomerSaysSilver(){
-		assertThat(welcomer.sayNeedGold(), containsString("gold"));
-	}
-	@Test
-	public void welcomerSaysSomething(){
-		assertThat(welcomer.saySome(), containsString("something"));
-	}
+   @Test
+   public void welcomerSaysWelcome() {
+   	assertThat(welcomer.sayWelcome(), containsString("Welcome"));
+   }
+   @Test
+   public void welcomerSaysFarewell() {
+   	assertThat(welcomer.sayFarewell(), containsString("Farewell"));
+   }
+   @Test
+   public void welcomerSaysHunter() {
+   	assertThat(welcomer.sayWelcome(), containsString("hunter"));
+   	assertThat(welcomer.sayFarewell(), containsString("hunter"));
+   	assertThat(welcomer.sayHunter(), containsString("hunter"));
+   }
+   @Test
+   public void welcomerSaysSilver(){
+   	assertThat(welcomer.sayNeedGold(), containsString("gold"));
+   }
+   @Test
+   public void welcomerSaysSomething(){
+   	assertThat(welcomer.saySome(), containsString("something"));
+   }
 }
